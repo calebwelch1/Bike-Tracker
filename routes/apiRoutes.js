f56da1f1b1858a23c0c7a82ea1984e3acb70518f
calebwelch <caleb.welch11@gmail.com>
2020-06-12 15:46:25 -0400

    changed folder structure, exported routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,10 @@
 // because the routes are created somewhere else than the main server we have to initialize what we need here then export
 const router = require("express").Router();
+// require express then call Router function on it
+//router creates a new ROUTER OBJECT that we can use
+
+// adding routes
+router.get("/", (request, response) => {});
+
+// then we export it to the main file
+module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,4 @@
 const router = require("express").Router();
 
+// html route to homepage
 module.exports = router;
